6c735866ec408eabda0b9fc4445c6a5ca7c918ba
nicolas9909 <nicolico99@hotmail.com>
2019-12-11 00:36:39 +0000

    task 13 Fix 1

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * insert_node - Insert a node in a singed linked list
+ *
+ * @head: Pointer to list to add the node
+ * @number: number to add to the list
+ * Return: Always 0.
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *tmp;
@@ -13,13 +20,13 @@ listint_t *insert_node(listint_t **head, int number)
 	new->n = number;
 	node = *head;
 	tmp = node->next;
-	if (*head == NULL)
+	if (!*head || number < node->n)
 	{
+		new->next = *head;
 		*head = new;
-		new->next = NULL;
-		return(new);
+		return (new);
 	}
-	while(tmp && number > tmp->n)
+	while (tmp && number > tmp->n)
 	{
 		node = tmp;
 		tmp = tmp->next;
