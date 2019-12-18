a61dc9cfdb11dc48af91ebcd43c55a1ba2916969
Nesgomez9 <nicolico99@hotmail.com>
2019-12-18 03:04:57 +0000

    Fixing task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    space = ""
     for i in matrix:
         for j in i:
-            print("{:d} ".format(j), end='')
+            print("{:s}{:d}".format(space, j), end='')
+            space = " "
         print("")
