6c4040128058ad99a64f2bb10bedae975500a20d
Nesgomez9 <nicolico99@hotmail.com>
2019-12-18 04:51:07 +0000

    task 13 last fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,23 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
-/**
- * list_len - check the code for Holberton School students.
- *
- * @h: h
- * Return: Always 0.
- */
-int list_len(const listint_t *h)
-{
-	int size = 0;
-
-	while (h)
-	{
-		h = h->next;
-		size++;
-	}
-	return (size);
-}
 /**
  * is_palindrome - check the code for Holberton School students.
  *
@@ -27,15 +7,18 @@ int list_len(const listint_t *h)
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp1, *tmp2;
-	int i, size, size1, count;
+	int i, size = 0;
 
 	if (!head)
 		return (0);
 	if (!*head)
 		return (1);
-	tmp1 = *head;
-	count = 0;
-	size = size1 = list_len(tmp1);
+	tmp1 = tmp2 = *head;
+	while (tmp2)
+	{
+		tmp2 = tmp2->next;
+		size++;
+	}
 	while (tmp1->next)
 	{
 		tmp2 = *head;
@@ -49,7 +32,6 @@ int is_palindrome(listint_t **head)
 			return (0);
 		tmp1 = tmp1->next;
 		size--;
-		count++;
 	}
 	return (1);
 }
