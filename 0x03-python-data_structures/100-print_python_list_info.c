d1ae67903cd843cc8ddc9672bff3a1d94691d60e
Nesgomez9 <nicolico99@hotmail.com>
2019-12-18 04:03:14 +0000

    adding fist advanced task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,15 +1,28 @@
-#include <Python.h>
 #include <stdio.h>
+#include <Python.h>
+
 /**
- * print_python_list_info - Prints some basic info about Python lists
- * @p: Pointer to python struct
+ * print_python_list_info - prints some basic info about Python lists
+ *
+ * @p: Object of python
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 void print_python_list_info(PyObject *p)
 {
-	unsigned int i;
+	long int size, alloc, i;
+        PyObject *item;
 
-	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
 
-	for (i = 0; i < PyList_Size(p); i++)
-		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+
+	}
+}
