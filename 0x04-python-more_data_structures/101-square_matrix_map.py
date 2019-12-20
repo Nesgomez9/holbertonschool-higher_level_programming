81ab5a3289c888bd0b6dbb18fef16b952c61db6c
Nesgomez9 <nicolico99@hotmail.com>
2019-12-20 04:28:40 +0000

    Fixing pep8 Style

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda y: y**2, row)),matrix))
+    return list(map(lambda row: list(map(lambda y: y**2, row)), matrix))
