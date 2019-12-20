7036eba463467a106a475870455791f682b49e47
Nesgomez9 <nicolico99@hotmail.com>
2019-12-20 03:54:11 +0000

    task 12 fix 2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str and not roman_string:
+    if type(roman_string) == str and roman_string:
+        dict_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+        suma = a = 0
+        roman_list = []
+        for count in roman_string:
+            for key in dict_num.keys():
+                if count == key:
+                    roman_list.append(dict_num[key])
+        for i, count in enumerate(roman_list):
+            if roman_list[i] > roman_list[i - 1] and i > 0:
+                suma += count - (roman_list[i - 1] * 2)
+            else:
+                suma += count
+        return suma
+    else:
         return 0
-    suma = a = 0
-    roman_list = []
-    for count in roman_string:
-        if count == 'I':
-            roman_list.append(1)
-        elif count == 'V':
-            roman_list.append(5)
-        elif count == 'X':
-            roman_list.append(10)
-        elif count == 'L':
-            roman_list.append(50)
-        elif count == 'C':
-            roman_list.append(100)
-        elif count == 'D':
-            roman_list.append(500)
-        elif count == 'M':
-            roman_list.append(1000)
-    for i, count in enumerate(roman_list):
-        if roman_list[i] > roman_list[i - 1] and i > 0:
-            suma += count - (roman_list[i - 1] * 2)
-        else:
-            suma += count
-    return suma
