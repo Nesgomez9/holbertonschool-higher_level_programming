b218c4fa9f04797e63e1cee522b56959b8625abc
Nesgomez9 <nicolico99@hotmail.com>
2020-01-10 04:36:13 +0000

    adding task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+""" Class Square tat defines  square whit a size"""
+    def __init__(self, size):
+        """Class Constructor that defines the atributtes of a square"""
+        self.__size = size
