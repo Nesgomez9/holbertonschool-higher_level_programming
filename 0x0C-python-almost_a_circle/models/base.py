fe8ac4a40023f1d97cb763d68809558a6fcf2647
Nesgomez9 <nicolico99@hotmail.com>
2020-01-27 21:47:04 +0000

    Adding task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,8 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """ Method that save the representation ob a object in a json into
+        a file """
         obj_list = []
         if list_objs:
             for i in list_objs:
@@ -33,3 +35,35 @@ class Base:
         s = cls.to_json_string(obj_list)
         with open("%s.json" % cls.__name__, "w", encoding="utf-8") as file1:
             file1.write(s)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Convert a JSON string into a python dictionary """
+        if not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns a new instance with the attributes that are defines
+        the dictionary """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(10,10)
+        elif cls.__name__ == "Square":
+            dummy = cls(10)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ Return a list of instance of a file"""
+        try:
+            with open("%s.json" % cls.__name__, encoding="utf-8") as file1:
+                r = file1.read()
+                dic = cls.from_json_string(r)
+                list1 = []
+                for i in dic:
+                    list1.append(cls.create(**i))
+                return list1
+        except IOError:
+            return []
