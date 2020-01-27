b42eceab34190eb1b2e8741b7659c051f2cb9804
Nesgomez9 <nicolico99@hotmail.com>
2020-01-27 19:34:10 +0000

    Adding task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,4 +20,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Method that returns the JSON string of a dictionary"""
+        if not list_dictionaries:
+            return "[]"
         return json.dumps(list_dictionaries)
