489cf059e72cd4e60edc00862fc1c061da71c195
Nesgomez9 <nicolico99@hotmail.com>
2020-03-04 03:23:44 +0000

    Fixing task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- lists the permissions of a user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- lists the permissions of a user_0d_1
+-- and user_0d_2
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
