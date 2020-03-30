9121baf274078ba80277c009f167707440effc6c
Nesgomez9 <nicolico99@hotmail.com>
2020-03-30 03:54:35 +0000

    [Feature] Fixing City class

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,4 +10,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
